a0dd76c29ce0ffcacecca07858e1f2fcdb0f5bf4
manojd <manoj.ducher@gmail.com>
2017-12-16 01:14:46 +0100

    Added python script to fetch follower number from twitter

---
+++ script.py
@@ -0,0 +1,14 @@
+from lxml import html
+import requests
+
+page = requests.get('https://twitter.com/emmanuelmacron')
+tree = html.fromstring(page.content)
+followers_list = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[3]/text()')
+
+followers='text'
+followers=followers_list[0]
+followers=followers.replace('\xa0', ' ')
+
+print(followers)
+
+
