03e2f8f2197e4889c55075aa2288eaf26bd1ecda
manojd <manoj.ducher@gmail.com>
2017-12-29 23:34:33 +0530

    Added new file create_table.py. This code creates a table followers having two columns into the database twitter_followers. The first column contains the date and time at which the number of followers was fetched. The second column contains the number of followers.

--- script.py
+++ script.py
@@ -5,10 +5,12 @@ page = requests.get('https://twitter.com/emmanuelmacron')
 tree = html.fromstring(page.content)
 followers_list = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[3]/text()')
 
-followers='text'
-followers=followers_list[0]
-followers=followers.replace('\xa0', ' ')
+print(followers_list)
 
-print(followers)
+#followers='text'
+#followers=followers_list[0]
+#followers=followers.replace('\xa0', ' ')
+
+#print(followers)
 
 
