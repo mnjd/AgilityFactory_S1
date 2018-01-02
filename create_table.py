cbca76fcb169f5dfb9423156f846c883f782c8cb
manojd <manoj.ducher@gmail.com>
2018-01-02 16:38:47 +0530

    Code improved to collect number of followers and create a table in database.

--- create_table.py
+++ create_table.py
@@ -6,32 +6,25 @@ import requests
 page = requests.get('https://twitter.com/emmanuelmacron')
 tree = html.fromstring(page.content)
 followers_list = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[3]/text()')
-followers_number='text'
-followers_number=followers_list[0]
+followers_number=''.join(followers_list)
 
 ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+conn = psycopg2.connect(database="twitter_followers", user="postgres", password="yusi8me")
+print("Opened twitter_followers database successfully")
+cur = conn.cursor()
 
 try:
-
-    conn = psycopg2.connect(database="twitter_followers", user="postgres", password="yusi8me")
-    print("Opened twitter_followers Database Successfully")
-    cur = conn.cursor()
     cur.execute("CREATE TABLE followers(Dateheure TIMESTAMP, followers_number VARCHAR(5))")
+except psycopg2.ProgrammingError:
+    print("Table already exists")
+except:
+    print("Unexpected error")
+    raise
+
+if conn:
+    conn.rollback()
+    cur = conn.cursor()
     cur.execute("INSERT INTO followers VALUES (%s, %s);", (ts, followers_number))
-
+    print('Table updated')
     conn.commit()
-
-#except psycopg2.DatabaseError, e:
-#    
-#    if con:
-#        con.rollback()
-#    
-#    print 'Error %s' % e    
-#    sys.exit(1)
-
-finally:
-    
-    if conn:
-        conn.close()
-
-print("Table created")
+    conn.close()
