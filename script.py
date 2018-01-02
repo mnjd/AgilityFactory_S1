a675a721ea4659e9831496a21d86302db986e1f5
manojd <manoj.ducher@gmail.com>
2018-01-02 16:49:53 +0530

    Updated script.py

--- script.py
+++ script.py
@@ -1,16 +1,30 @@
+import psycopg2
+import datetime
 from lxml import html
 import requests
 
 page = requests.get('https://twitter.com/emmanuelmacron')
 tree = html.fromstring(page.content)
 followers_list = tree.xpath('//*[@id="page-container"]/div[1]/div/div[2]/div/div/div[2]/div/div/ul/li[3]/a/span[3]/text()')
+followers_number=''.join(followers_list)
 
-print(followers_list)
+ts=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+conn = psycopg2.connect(database="twitter_followers", user="postgres", password="yusi8me")
+print("Opened twitter_followers database successfully")
+cur = conn.cursor()
 
-#followers='text'
-#followers=followers_list[0]
-#followers=followers.replace('\xa0', ' ')
+try:
+    cur.execute("CREATE TABLE followers(Dateheure TIMESTAMP, followers_number VARCHAR(5))")
+except psycopg2.ProgrammingError:
+    print("Table already exists")
+except:
+    print("Unexpected error")
+    raise
 
-#print(followers)
-
-
+if conn:
+    conn.rollback()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO followers VALUES (%s, %s);", (ts, followers_number))
+    print('Table updated')
+    conn.commit()
+    conn.close()
